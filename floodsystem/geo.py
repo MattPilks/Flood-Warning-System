694c9528baa2eba5f5d68337a60340030c1b4457
Matt Pilkington <mp2017@cam.ac.uk>
2022-02-08 22:47:11 +0000

    First Commint

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,15 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+from haversine import haversine
+
+def stations_within_radius(stations,centre,r):
+    valid_stations = []
+    
+    for station in stations:
+       distance = haversine(station.coord, centre)
+       if distance <= r:
+        valid_stations.append(station.name)
+        sorted_valid_stations = sorted(valid_stations)
+    return(sorted_valid_stations)
