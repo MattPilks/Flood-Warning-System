4df4d14c1a957cd3aee6fd35565bcedb17090168
ljb217 <93184489+ljb217@users.noreply.github.com>
2022-02-09 02:30:02 +0000

    Update Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -1,1 +1,19 @@
-stations = 
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+def run():
+  """Requiremtn for task B"""
+  stations = build_station_list()
+  station_list = []
+  for station in stations:
+    station_name = station.name
+    station_coord = station.coord
+    station_list.append(station_name, station_coord)
+   sorted = stations_by_distance(station_list,(52.2053, 0.1218))
+  close_ten = sorted[:10]
+  farth_ten = sorted[10:]
+  print(close_ten)
+  print(farth_ten)
+  
+  if __name__ == "__main__":
+    print("Ten closest and ten furthest stations from cambridge city centre are:")
+    run()
