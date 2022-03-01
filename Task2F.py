9f1a6959e7401b8617117f95fdcd7e6da4a402ed
Matt Pilkington <mp2017@cam.ac.uk>
2022-03-01 21:14:21 +0000

    2F

---
+++ Task2F.py
@@ -0,0 +1,41 @@
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+from datetime import datetime, timedelta
+from floodsystem.plot import plot_water_level_with_fit
+
+"""Requirements for Task2F"""
+
+
+
+
+def run():
+    stations = build_station_list()
+    if DEBUG: print("Build_station_list len:", len(stations))
+    if not FULL_LIST: stations = stations[0:10]
+    station_dict = {}
+    highest_level_stations = []
+    for station in stations:
+        if DEBUG: print("fetching measure level for current station:", station.name, station.measure_id)
+        date_list , level_list = fetch_measure_levels(station.measure_id, timedelta(hours=24))
+        station_dict[station.name] = level_list[0] if len(level_list) > 0 else 0
+        if DEBUG: print("measure level for current station:", station_dict[station.name])
+    sorted_list = sorted(station_dict.items(), key = lambda x: x[1], reverse=True)
+    if DEBUG: print("sorted_list: ", sorted_list)
+    for item in sorted_list:
+        highest_level_stations.append(item[0])
+    highest_five_stations = highest_level_stations[0:5]
+    print("highest_five_stations: ", highest_five_stations)
+
+    for station in stations:
+        for topstation in highest_five_stations:
+            if station.name == topstation:
+                if DEBUG: print("fetching measure level for current station:", station.name, station.measure_id)
+                date_list , level_list = fetch_measure_levels(station.measure_id, timedelta(days=2))
+                plot_water_level_with_fit(station,date_list,level_list,4)
+
+if __name__ == "__main__":
+    DEBUG = False
+    FULL_LIST = False
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    run()
+    
