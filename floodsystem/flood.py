f4043666781f3b556c2b90f36669c270e586c12b
Louis Bullock <ljb217@cam.ac.uk>
2022-03-01 20:48:48 +0000

    continued work on 2B

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,4 +1,4 @@
-
+from operator import itemgetter
 
 def stations_level_over_threshold(stations,tol):
     stations_over = tuple()
@@ -7,4 +7,6 @@ def stations_level_over_threshold(stations,tol):
         if station.relative_water_level > tol:
             
             stations_over.append((station, station.relative_water_level))
+    
+    stations_over = sorted(stations_over, key=itemgetter(1))
 
