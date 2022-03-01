c0aa5aeaf2d46d756257128cc07fdb334f3d5ff7
Louis Bullock <ljb217@cam.ac.uk>
2022-03-01 20:36:14 +0000

    stations over threshold initialised

--- flood.py
+++ flood.py
@@ -1,2 +1,10 @@
+
+
 def stations_level_over_threshold(stations,tol):
-    
+    stations_over = tuple()
+    stations_over = []
+    for station in stations:
+        if station.relative_water_level > tol:
+            
+            stations_over.append((station, station.relative_water_level))
+
